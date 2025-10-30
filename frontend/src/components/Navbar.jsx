6ebc8c4cfa47c3b07f6036f90cb6119fb5347789
Abhimaan Kumar <Abhimaan2kumar@gmail.com>
2025-10-30 10:37:14 +0530

    fond added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 bg-black/50 backdrop-blur border-b border-white/5">
       <div className="h-16 flex items-center justify-between p-3">
-          <div className="font-['Lobster',cursive] text-4xl italic tracking-wide">Humor Check</div>
+          <div className="font- ['Trade+Winds', system-ui] text-4xl italic tracking-wide">Humor Check</div>
         <nav className="flex items-center">
           <a className="text-slate-400 ml-4 text-xl hover:text-white hover:bg-white/20 rounded-md px-2 py-1" href="#">Account</a>
           <a className="text-slate-400 ml-4 text-xl hover:text-white hover:bg-white/20 rounded-md px-2 py-1" href="#">Friends</a>
